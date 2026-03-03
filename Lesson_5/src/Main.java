f260737a0b0a4136f8927f7c58fc4789a3e6f686
Keyrenjddd <spbs.exes@gmail.com>
2026-03-03 16:43:15 +0300

    Add files via upload

---
+++ Lesson_5/src/Main.java
@@ -0,0 +1,77 @@
+public class Main {
+
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4, а у него строк: " + array.length);
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Массив должен быть размером 4x4, а в строке " + i + " длина " + array[i].length);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int value = Integer.parseInt(array[i][j]);
+                    sum += value;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("В ячейке [" + i + "][" + j + "] лежит нечисловое значение: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        String[][] wrongSizeArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}
+        };
+
+        String[][] wrongDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "abc"}
+        };
+
+        try {
+            int sum = processArray(correctArray);
+            System.out.println("Сумма корректного массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int sum = processArray(wrongSizeArray);
+            System.out.println("Сумма: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int sum = processArray(wrongDataArray);
+            System.out.println("Сумма: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("\n--- Демонстрация ArrayIndexOutOfBoundsException ---");
+        try {
+            int[] arr = new int[5];
+            int x = arr[10];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Поймано исключение: " + e);
+        }
+    }
+}
